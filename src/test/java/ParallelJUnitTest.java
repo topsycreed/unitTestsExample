d934a36ca857ee6eb2ab5875a1d9fdd83b0de5ff
gchursov <topsycreed@gmail.com>
2023-12-17 21:37:12 +0100

    Add examples of parallel tests

---
+++ src/test/java/ParallelJUnitTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.*;
+
+public class ParallelJUnitTest {
+
+    @Test
+    public void first() throws Exception{
+        System.out.println("FirstParallelUnitTest first() start => " + Thread.currentThread().getName());
+        Thread.sleep(500);
+        System.out.println("FirstParallelUnitTest first() end => " + Thread.currentThread().getName());
+    }
+
+    @Test
+    public void second() throws Exception{
+        System.out.println("FirstParallelUnitTest second() start => " + Thread.currentThread().getName());
+        Thread.sleep(500);
+        System.out.println("FirstParallelUnitTest second() end => " + Thread.currentThread().getName());
+    }
+}
